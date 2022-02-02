b062982e38d379ac1ee05bc3ef77d107ffebe3c3
anchor0301 <wp2102@kakao.com>
2022-02-02 22:42:02 +0900

    2월2일 사라진 코드들 복구

--- mix_3/main.py
+++ mix_3/main.py
@@ -1,10 +1,51 @@
-from gspread_API_1 import test as gsp
-from selenium_2 import sele_test as sele
-import gspread
-import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from oauth2client.service_account import ServiceAccountCredentials
 
+import re
+import gspread
+import time
+import chromedriver_autoinstaller
+
 start = time.time()  ################## 기록시작
+print("기록을 시작합니다.")
+
+###############################    셀리니움 코드    ################################################
+
+chromedriver_autoinstaller.install()
+
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+driver = webdriver.Chrome(options=chrome_options)
+url = "https://contacts.google.com/new?hl=ko"
+driver.get(url)
+
+
+# 이름 추가
+# 애견이름/견종/서비스/전화번호
+def reg_profile(name):
+    add_name = driver.find_element_by_xpath("//*[@id='c0']/div[2]/div[1]/div/div[1]/div/div[1]/input")
+    add_name.send_keys(name)
+
+
+# 전화번호 입력
+def reg_numbers(phone_number):
+    add_num = driver.find_element_by_xpath("//*[@id='c5']/div[1]/div[2]/div[1]/div/div[1]/input")
+    add_num.send_keys(phone_number)
+
+
+def registers():
+    register = driver.find_element_by_xpath(
+        " //*[@id='yDmH0d']/c-wiz/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/button ")
+    register.click()
+
+###############################    gpread코드    ##############################################################
+# 연락처 가져오기
+def get_num(cell):
+    num = worksheet.acell("f" + cell).value
+    name = worksheet.acell("d" + cell).value
+    print(name + "회원님의 전화번호는 [" + num + "] 입니다.")
 scope = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive',
@@ -18,13 +59,42 @@ doc = gc.open_by_url(spreadsheet_url)
 # 시트 선택하기
 worksheet = doc.worksheet('시트1')
 
+column_data = worksheet.col_values(6)
+
+
+# 제일 마지막 회원 이름
+def last_name():
+    cell_data = worksheet.acell("d" + str(len(column_data))).value
+    print(cell_data)
+    return cell_data
+
+
+#  i 애견이름/j 견종/a 서비스/f 전화번호
+def last_info():
+    dog_name = worksheet.acell("i" + str(len(column_data))).value
+    dog_breed = worksheet.acell("j" + str(len(column_data))).value
+    service = worksheet.acell("a" + str(len(column_data))).value
+    phone_numbers = worksheet.acell("f" + str(len(column_data))).value
+
+    # 서비스 첫글자
+    # 괄호안의 글자 삭제
+    rm_breed = re.sub(r'\([^)]*\)', '', dog_breed)
+    # 출력
+    print_last_info = f"{dog_name}/{rm_breed.rstrip()}/{service[0]}/{phone_numbers[7:]}"
+    return print_last_info
+
+
+# 제일 마지막 회원 전화번호
+def last_num():
+    cell_data = worksheet.acell("f" + str(len(column_data))).value
+    return cell_data
 
 
 def regster():
     # 최신 고객의 이름등록
-    sele.reg_profile(gsp.last_info())
+    reg_profile(last_info())
     # 최신 고객의 전화번호 등록
-    sele.reg_numbers(gsp.last_num())
+    reg_numbers(last_num())
     print("등록 완료")
     # 등록하기
     # sele.registers()
