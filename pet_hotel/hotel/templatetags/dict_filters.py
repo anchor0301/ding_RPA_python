4bd353e86ef34900ebfa1bf553253343a64ef056
anchor <wp2102@kakao.com>
2025-05-04 00:06:30 +0900

    feat: Kanban 카드에 강아지 정보 및 고객 전화번호 뒷자리 표시
    - 카드 제목: 강아지 이름 / ***-****-1234 / 몸무게 형식으로 개선
    - 템플릿 필터 `last4` 추가하여 전화번호 뒷자리만 출력되도록 구현
    - 기존 card-title 텍스트 구조 정리 및 가독성 향상

--- pet_hotel/hotel/templatetags/dict_filters.py
+++ pet_hotel/hotel/templatetags/dict_filters.py
@@ -4,3 +4,8 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, [])
+
+
+@register.filter
+def last4(phone_number):
+    return phone_number[-4:] if phone_number else ""
